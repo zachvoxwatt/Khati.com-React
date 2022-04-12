26d5a088015fcb6f0ec50bc47c6b8959934d07e2
Võ C. Kha <53788250+ZeDamien2K@users.noreply.github.com>
2022-04-12 17:14:13 +0700

    v0.5
    - Added tab icons for various browsers and devices
    - Added some basic sounds
    - Completed the Frontend

--- src/scripts/Buttons.js
+++ src/scripts/Buttons.js
@@ -1,9 +1,50 @@
+import React, {useState} from "react"
+import '../css/buttons.css'
+import generateSound from '../resources/audio/generate.mp3'
+import clipboardSound from '../resources/audio/copy.mp3'
+
+const timeoutArray = [null, null]
+
 const Buttons = function ()
 {
+    const inigen = 'Generate Emojis'
+    const postgen = "Don't like it? Have another pile!"
+    const iniclip = 'Jam it to Clipboard'
+    const postclip = 'Copied to Clipboard!'
+
+    const [generateButtonState, setGButtonState] = useState(inigen)
+    const [clipboardButtonState, setCBoardButtonState] = useState(iniclip)
+    
+    function generateEmojis()
+    {
+        setGButtonState(postgen)
+
+        clearInterval(timeoutArray[0])
+        timeoutArray[0] = setInterval(function () { setGButtonState(inigen) }, 3000)
+        playSound(generateSound)
+    }
+
+    function giveClipboardData()
+    {
+        
+        setCBoardButtonState(postclip)
+
+        clearInterval(timeoutArray[1])
+        timeoutArray[1] = setInterval(() => { setCBoardButtonState(iniclip) }, 3000)
+        playSound(clipboardSound)
+    }
+
+    function playSound(file)
+    {
+        let audio = new Audio(file)
+        audio.volume = 0.02
+        audio.play()
+    }
+
     return(
-        <div id='buttons-box'>
-            <button defaultValue={'A'}></button>
-            <button name={'Jam it to Clipboard'}></button>
+        <div className='buttons-box'>
+            <button id='generateButton' onClick={() => generateEmojis()}>{generateButtonState}</button>
+            <button id='clipboardButton' onClick={() => giveClipboardData()}>{clipboardButtonState}</button>
         </div>
     )
 }
--- src/scripts/InputBox.js
+++ src/scripts/InputBox.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import React from "react"
 import '../css/box.css'
 
 class InputBox extends React.Component
@@ -6,20 +6,41 @@ class InputBox extends React.Component
     constructor(props)
     {
         super(props)
-        this.inistate = {value : 'Put some emojis here to customize the output. If no value presents, default preset is used. (Max value: 40)'}
+        this.inistate = 
+        {
+            normalnote : 'Put some emojis here to customize the output. If no value presents, default preset is used.', 
+            warningnote: "Note: Only emojis are allowed. Alphabetical letters, numeric digits or special characters are not supported."
+        }
+
+        this.customEmojis = {value : []}
+    }
+
+    displayNotice(event, boolean)
+    {
+        if (boolean) event.target.placeholder = this.inistate.warningnote
+        else event.target.placeholder = this.inistate.normalnote
     }
 
     checkInput(event)
     {
+        let customInputs = event.target.value
+        let splittedInput = customInputs.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/)
+        let outputArray = []
+
+        for (let i = 0; i < splittedInput.length; i++)
+        {
+            let indexChar = splittedInput[i]
+            if (indexChar !== "") outputArray.push(indexChar)
+        }
+
+        this.customEmojis.value = outputArray
     }
 
     render()
     {
         return(
             <div id='inputbox-box'>
-                <textarea onChange={(event) => this.checkInput(event)} id='inputbox' rows={1} cols={16} placeholder={this.inistate.value}></textarea>
-                
-                <h6 id='status'>hello</h6>
+                <textarea onChange={(event) => this.checkInput(event)} onMouseOver={(event) => this.displayNotice(event, true)} onMouseLeave={(event) => this.displayNotice(event, false)} id='inputbox' rows={3} cols={16} placeholder={this.inistate.normalnote}></textarea>
             </div>
         )
     }
